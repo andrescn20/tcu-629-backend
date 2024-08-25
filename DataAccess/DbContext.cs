39bfd549a537fef4031fc53757a11e52634e4d16
Andres Castro <andrescn20@gmail.com>
2024-08-25 16:10:33 -0600

    Update on delete behaviors

--- DataAccess/DbContext.cs
+++ DataAccess/DbContext.cs
@@ -23,8 +23,6 @@ namespace DataAccess
             modelBuilder.Entity<TemperatureData>(entity =>
             {
                 entity.HasKey(e => e.Id);
-
-                // Specify the precision and scale for decimal properties
                 entity.Property(e => e.Temperature)
                       .HasColumnType("decimal(5, 2)");
 
@@ -38,7 +36,8 @@ namespace DataAccess
             modelBuilder.Entity<TemperatureData>()
                 .HasOne(td => td.Sensor)
                 .WithMany(s => s.TemperatureDataList)
-                .HasForeignKey(td => td.SensorId);
+                .HasForeignKey(td => td.SensorId)
+                .OnDelete(DeleteBehavior.Restrict);
 
             modelBuilder.Entity<DispenserData>()
                 .HasOne(td => td.Sensor)
@@ -53,17 +52,20 @@ namespace DataAccess
             modelBuilder.Entity<Sensor>()
                 .HasOne(s => s.Board)
                 .WithMany(b => b.Sensors)
-                .HasForeignKey(s => s.BoardId);
+                .HasForeignKey(s => s.BoardId)
+                .OnDelete(DeleteBehavior.SetNull);
 
             modelBuilder.Entity<Sensor>()
                 .HasOne(s => s.SensorType)
                 .WithMany(st => st.Sensors)
-                .HasForeignKey(s => s.SensorTypeId);
+                .HasForeignKey(s => s.SensorTypeId)
+                .OnDelete(DeleteBehavior.Restrict);
 
             modelBuilder.Entity<Board>()
                 .HasOne(b => b.Device)
                 .WithMany(d => d.Boards)
-                .HasForeignKey(b => b.DeviceId);
+                .HasForeignKey(b => b.DeviceId)
+                .OnDelete(DeleteBehavior.SetNull);
         }
 
     }
