860be26a2e07147a8a5db868f058cc0b81ca6056
Andrés Castro <andrescn20@gmail.com>
2024-09-07 15:14:16 -0600

    Updated collected data interfaces

--- API/Interfaces/ICollectedDataService.cs
+++ API/Interfaces/ICollectedDataService.cs
@@ -6,6 +6,9 @@ namespace API.Interfaces
     public interface ICollectedDataService
     {
         Task<TemperatureDataDto> AddTemperatureDataAsync(TemperatureDataDto sensorData);
+        Task<List<TemperatureDataDto>> GetTemperatureDataByDeviceId(int deviceId);
+        Task<List<TemperatureDataDto>> GetTemperatureDataBySensorId(int sensorId);
+        Task<List<TemperatureDataDto>> GetAllTemperatureMeasurements();
     }
 }
 
--- DataAccess/Interfaces/ICollectedDataRepository.cs
+++ DataAccess/Interfaces/ICollectedDataRepository.cs
@@ -5,6 +5,9 @@ namespace DataAccess.Interfaces
     public interface ICollectedDataRepository
     {
         Task<TemperatureData> SaveTemperatureSensorDataAsync(TemperatureData temperatureData);
+        Task<Device> GetTemperatureDataByDeviceIdAsync(int deviceId);
+        Task<Sensor> GetTemperatureDataBySensorIdAsync(int sensorId);
+        Task<List<TemperatureData>> GetAllTemperatureMeasurementsAsync();
 
     }
 }
