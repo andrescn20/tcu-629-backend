fec55b29f4a27c5468e74390fc8e939c051c26b5
Andrés Castro <andrescn20@gmail.com>
2024-09-07 15:16:41 -0600

    Add Identity User

---
+++ DataAccess/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace DataAccess.Models
+{
+
+    public class ApplicationUser : IdentityUser
+    {
+        // Add custom properties here if needed
+    }
+}
