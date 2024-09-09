6881bb4d633a6f4a60f98f2cf025a84067e13006
Andrés Castro <andrescn20@gmail.com>
2024-09-08 21:26:10 -0600

    Dtos para manejo de usuarios y contraseñas

---
+++ DTO/PassWordDto.cs
@@ -0,0 +1,16 @@
+﻿
+namespace DTO
+{
+    public class ForgotPasswordDto
+    {
+        public string Email { get; set; }
+        public string ResetUrl { get; set; }
+    }
+
+    public class ResetPasswordDto
+    {
+        public string Email { get; set; }
+        public string Token { get; set; }
+        public string Password { get; set; }
+    }
+}
